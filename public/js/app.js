cf954d1cfc6f9cf0831b6d32b6665bc5790ec4af
Ovsk777 <abhishekpal92@yahoo.co.in>
2021-03-07 00:35:19 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const location = search.value;
     messagOne.textContent = 'Loading...';
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{     //fetch method for requesting can only be used in client side js and not server side js
+    fetch('/weather?address='+location).then((response)=>{     //fetch method for requesting can only be used in client side js and not server side js
     response.json().then((data)=>{                                               // hence we cannot run this fn. in node 
        if(data.error){
         messagOne.textContent = data.error
